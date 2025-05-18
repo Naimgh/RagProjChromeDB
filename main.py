ef39937c832ba84e1b907c1c6d4baa1253fa302d
Naimgh <naim.ghaith@mobileartsme.com>
2025-05-19 01:28:35 +0300

    first commit

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-#from vector import retriever
 from vectorelocalmemory import retriever
-
+## using llama3.2 as a LLM 
 model = OllamaLLM(model="llama3.2")
-
-#template = """
-#You are an exeprt in answering questions about a pizza restaurant
-
-#Here are some relevant reviews: {reviews}
-
-#Here is the question to answer: {question}
-#"""
-#prompt = ChatPromptTemplate.from_template(template)
-
+## using the template for the chat anwer by the LLM
 template = """Use the following pieces of context to answer the question at the end. Please follow the following rules:
 1. If you don't know the answer, don't try to make up an answer. Just say "I can't find the final answer but you may want to check the following links".
 2. If you find the answer, write the answer in a concise way with five sentences maximum.
@@ -27,7 +17,7 @@ Helpful Answer:
 prompt = ChatPromptTemplate.from_template(template)
 
 
-
+## concatenate the context with the answer using chain free library
 chain = prompt | model
 
 while True:
@@ -40,3 +30,4 @@ while True:
     context = retriever.invoke(question)
     result = chain.invoke({"context": context, "question": question})
     print(result)
+
--- vectorelocalmemory.py
+++ vectorelocalmemory.py
@@ -7,31 +7,31 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 import pandas as pd
 from urllib.request import urlretrieve
-
+## i have downloaded ollam on my laptop and i have used this model for embedding
 embeddings = OllamaEmbeddings(model="mxbai-embed-large")
-
+## using Chroma to save in local data base instead of memory
 db_location = "./chrome_langchain_db"
 add_documents = not os.path.exists(db_location)
-
+## chunk the data
 text_splitter =  RecursiveCharacterTextSplitter(
     chunk_size =700,
     chunk_overlap = 50
     )
 
-
+## check if the data base not create then go inside the below 
 if add_documents:
     documents = []
     ids = []
     
     loader = PyPDFDirectoryLoader("files-pdf/")
     docs_before_split = loader.load()
+    ## we split the data as the embedding model does not accept a big size of data
     docs_after_split = text_splitter.split_documents(docs_before_split)
    
-
-
+## save the document spliter into the vectore data base
 if add_documents:
     vectorstore = Chroma.from_documents(docs_after_split, embeddings, persist_directory="chrome_langchain_db")
-
+## retrive the data from the vector data base
 vector_store = Chroma(persist_directory="chrome_langchain_db", embedding_function=embeddings)   
 retriever = vector_store.as_retriever(
     search_kwargs={"k": 3}
